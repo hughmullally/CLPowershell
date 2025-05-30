d45645d92e0275ad6ee9ec7057712cdb3d826e98
Hugh Mullally <hugh.mullally@cubelogic.com>
2025-05-30 17:43:55 +0100

    Refactor clean folder code into a function CleanTargetFolder

--- src/Utils/CleanTargetFolders.ps1
+++ src/Utils/CleanTargetFolders.ps1
@@ -1,24 +1,27 @@
-y$Client = "Drax"
-$rootFolder = "C:\git\ClientReleases\$Client\Release"
+function CleanTargetFolder() {
+    param (
+        [string]$Client
+    )
+    $rootFolder = "C:\git\ClientReleases\$Client\Release"
+    y$confirmation = Read-Host "Are you sure you want to clean folder '$rootFolder'? (y/n)"
+    if ($confirmation -ne 'y') {
+        Write-Host "Operation cancelled"
+        exit
+    }
 
-
-$confirmation = Read-Host "Are you sure you want to clean folder '$rootFolder'? (y/n)"
-if ($confirmation -ne 'y') {
-    Write-Host "Operation cancelled"
-    exit
-}
-
-Write-Host "Cleaning target folder: $rootFolder"
-Get-ChildItem -Path $rootFolder -Recurse -File | ForEach-Object {
-    if ($_.DirectoryName -notlike $rootFolder) {       
-        try {
-            Write-Host "Deleting file: $($_.FullName)"
-            Remove-Item $_.FullName -Force
-        }
-        catch {
-            Write-Warning "Failed to delete file: $($_.FullName). Error: $_"
+    Write-Host "Cleaning target folder: $rootFolder"
+    Get-ChildItem -Path $rootFolder -Recurse -File | ForEach-Object {
+        if ($_.DirectoryName -notlike $rootFolder) {       
+            try {
+                Write-Host "Deleting file: $($_.FullName)"
+                Remove-Item $_.FullName -Force
+            }
+            catch {
+                Write-Warning "Failed to delete file: $($_.FullName). Error: $_"
+            }
         }
     }
+    Write-Host "Finished cleaning target folder"
 }
-Write-Host "Finished cleaning target folder"
 
+CleanTargetFolder 'Wintershall'
